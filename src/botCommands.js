9a21a0f3307bc7ac82738a6f4eb687c1e21d5d4d
Connor Slade <turtleslade@gmail.com>
2021-02-10 22:33:47 -0500

    Add random color

--- src/botCommands.js
+++ src/botCommands.js
@@ -164,6 +164,14 @@ module.exports = {
                 }
             }
         },
+        "randomcolor" : {
+            "help": common.embedMessage(color.help, 'Help: Random Color', 'Generates random color!\nUsage: `$randomcolor`'),
+            "usage": 'randomcolor',
+            process: function (msg, command) {
+                let hexCode = common.rgbToHex(Math.floor(Math.random() * 255) + 1, Math.floor(Math.random() * 255) + 1, Math.floor(Math.random() * 255) + 1);
+                msg.channel.send(common.embedMessage(hexCode, 'Random Color :paintbrush:', `**HEX:** \`${hexCode}\``));
+            }
+        },
         "say": {
             "help": common.embedMessage(color.help, 'Help: Say', 'Says Stuff\nUsage: `$say <text>`'),
             "usage": 'say <text>',
