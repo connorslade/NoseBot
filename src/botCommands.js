004419a8c24cd427e1ea5f44f898983594d96c2e
Connor Slade <turtleslade@gmail.com>
2021-02-19 00:31:00 -0500

    Finish Unix command

--- src/botCommands.js
+++ src/botCommands.js
@@ -43,16 +43,23 @@ module.exports = {
             "help": common.embedMessage(color.help, 'Help: bash', 'Gives Information on Bash Commands\nUsage: `$bash <commandName>`'),
             "usage": 'bash <commandName>',
             process: function (msg, command) {
-                if (typeof(global.unixCommands) !== 'undefined' && global.unixCommands) {
+                function doCommand(){
                     if (command.length === 1) {
-                        msg.channel.send(common.embedMessage(color.red, "PLACEHOLDER", "IM TOO LAZY TO DO THIS PART YET LOL"));//TODO:MAKE THIS SO STUFF
+                        msg.channel.send(common.embedMessage(color.red, "Unix Commands", "Use `$unix <commandName>` to get info on a UNIX command.\nAll commands from: https://wikipedia.org/wiki/List_of_Unix_commands"));
                     } else if (unixCommands.hasOwnProperty(command[1].toLowerCase())){
                         let infoOnCommand = command[1].toLowerCase();
-                        msg.channel.send(common.embedMessage(color.main, "Unix Command: `" + infoOnCommand + "`", unixCommands[infoOnCommand].description));//TODO:CLEAN UP
+                        let commandInfo = unixCommands[infoOnCommand];
+                        msg.channel.send(common.embedMessage(color.main, "Unix Command: `" + infoOnCommand + "`", `**${commandInfo.description}**\n\`\`\`Category: ${commandInfo.category}\nStatus: ${commandInfo.status}\`\`\``));
+                    }else{
+                        msg.channel.send(common.embedMessage(color.red, "Unknown Command...", "This is not a valid UNIX command...\nTry running `$unix` to see all unix commands!"));
                     }
+                }
+
+                if (typeof(global.unixCommands) !== 'undefined' && global.unixCommands) {
+                    doCommand();
                 }else {
                     global.unixCommands = JSON.parse(fs.readFileSync('./assets/unixCommands.json', 'utf8'));
-                    msg.channel.send(common.embedMessage(color.main, "Commands Loaded", ""))//TODO:REMOVE THIS
+                    doCommand();
                 }
             }
         },
