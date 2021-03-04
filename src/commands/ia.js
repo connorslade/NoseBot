c3f7e4d262f5a0d152dd62a3284f726e5ddd5ba5
Connor Slade <turtleslade@gmail.com>
2021-03-03 21:24:17 -0500

    Not Beta No More :)

--- src/commands/ia.js
+++ src/commands/ia.js
@@ -8,9 +8,9 @@ module.exports = {
         let working = command.join(' ');
         working = working.split('ia ')[1];
 
-        let Loading = await msg.channel.send(common.embedMessage(color.link, `Loading Instant AnswSer...`, 'Hang Tight!'));
+        let Loading = await msg.channel.send(common.embedMessage(color.link, `Loading Instant Answer...`, 'Hang Tight!'));
 
-            https.get(`https://api.duckduckgo.com/?q=${encodeURI(working)}&format=json&pretty=1&atb=v214-1`, (response) => {
+        https.get(`https://api.duckduckgo.com/?q=${encodeURI(working)}&format=json&pretty=1&atb=v214-1`, (response) => {
             let todo = '';
             response.on('data', (chunk) => {
                 todo += chunk;
@@ -19,25 +19,25 @@ module.exports = {
             response.on('end', () => {
                 let jsonResponse = JSON.parse(todo);
                 if (jsonResponse['Abstract'] === "" && jsonResponse['RelatedTopics'][0] === undefined) {
-                    Loading.edit(common.embedMessage(color.red, `**Search:** ${jsonResponse['Heading']} {BETA}`, `No Instant Answer for ${working}`));
+                    Loading.edit(common.embedMessage(color.red, `**Search:** ${jsonResponse['Heading']}`, `No Instant Answer for ${working}`));
                     return;
                 }
 
                 if (!(jsonResponse['Abstract'] === "")) {
                     let contents = jsonResponse['Infobox']['content'];
                     let footerContent = (contents !== undefined) ? `${contents[0]['label']} — ${contents[0]['value']}\n${contents[1]['label']} — ${contents[1]['value']}\n${contents[2]['label']} — ${contents[2]['value']}` : '';
-                    let bodyContent = `\`\`\`${jsonResponse['AbstractText'].split('.').slice(0,3).join('.')}...\`\`\``;
-                    Loading.edit(common.embedMessage(color.nose, `**Search:** ${jsonResponse['Heading']} {BETA}`, bodyContent).setURL(jsonResponse['AbstractURL']).setFooter(footerContent));
+                    let bodyContent = `\`\`\`${jsonResponse['AbstractText'].split('.').slice(0, 3).join('.')}...\`\`\``;
+                    Loading.edit(common.embedMessage(color.nose, `**Search:** ${jsonResponse['Heading']}`, bodyContent).setURL(jsonResponse['AbstractURL']).setFooter(footerContent));
                     return;
                 }
 
                 if (!(jsonResponse['RelatedTopics'][0] === undefined)) {
                     let bodyContent = `\`\`\`${jsonResponse['RelatedTopics'][0]['Text']}\`\`\``;
-                    if (jsonResponse['RelatedTopics'][0]['Icon']['URL'] === ""){
-                        Loading.edit(common.embedMessage(color.nose, `**Search:** ${jsonResponse['Heading']} {BETA}`, bodyContent).setURL(jsonResponse['AbstractURL']));
+                    if (jsonResponse['RelatedTopics'][0]['Icon']['URL'] === "") {
+                        Loading.edit(common.embedMessage(color.nose, `**Search:** ${jsonResponse['Heading']}`, bodyContent).setURL(jsonResponse['AbstractURL']));
                         return;
                     }
-                    Loading.edit(common.embedMessage(color.nose, `**Search:** ${jsonResponse['Heading']} {BETA}`, bodyContent).setURL(jsonResponse['AbstractURL']).setImage(`https://api.duckduckgo.com/${jsonResponse['RelatedTopics'][0]['Icon']['URL']}`));
+                    Loading.edit(common.embedMessage(color.nose, `**Search:** ${jsonResponse['Heading']}`, bodyContent).setURL(jsonResponse['AbstractURL']).setImage(`https://api.duckduckgo.com/${jsonResponse['RelatedTopics'][0]['Icon']['URL']}`));
                 }
 
             });
